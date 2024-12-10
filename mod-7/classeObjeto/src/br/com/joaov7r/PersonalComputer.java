3f724c12d197d2619cc6183d92654e9cb3e89971
joaov7r <jvc.lopes@outlook.com>
2024-12-10 13:31:14 -0300

    Module 7 task

---
+++ mod-7/classeObjeto/src/br/com/joaov7r/PersonalComputer.java
@@ -0,0 +1,16 @@
+package br.com.joaov7r;
+
+public class PersonalComputer {
+
+	//Essa clase contém os atributos do meu próprio computador
+	
+	private int ramMemoryQuantity;
+	private String videoCard;
+	private String motherBoard;
+	private String powerSupply;
+	private String waterCooler;
+	private int screenQauntity;
+	private String mouse;
+	private String keyboard;
+	private String mousepad;
+}
---
+++ mod-7/classeObjeto/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module classeObjeto {
+}
