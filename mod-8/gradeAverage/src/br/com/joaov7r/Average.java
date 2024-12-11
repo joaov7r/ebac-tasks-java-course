1df20a00bac243db162b24c2bea73e984451a8bf
joaov7r <jvc.lopes@outlook.com>
2024-12-11 12:20:16 -0300

    Transfer project to IntelliJ and Module 8 task

---
+++ mod-8/gradeAverage/src/br/com/joaov7r/Average.java
@@ -0,0 +1,14 @@
+package br.com.joaov7r;
+
+public class Average {
+	public static void main(String[] args) {
+		double grade1 = 10.0;
+		double grade2 = 7.3;
+		double grade3 = 3.5;
+		double grade4 = 9.1;
+
+		double avg = (grade1+grade2+grade3+grade4) / 4;
+
+		System.out.println("A média das 4 notas é: " + avg);
+	}
+}
